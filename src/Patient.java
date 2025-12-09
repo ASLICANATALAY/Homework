86de9fb63268c66ee085b69d60dd9ec87b87a01c
ASLICANATALAY <s230444052@stu.thk.edu.tr>
2025-12-09 19:29:48 +0300

    init commit

---
+++ src/Patient.java
@@ -0,0 +1,22 @@
+
+
+class Patient {
+    int id;
+    String name;
+    int severity;
+    int age;
+    Patient next;
+
+    public Patient(int id, String name, int severity, int age) {
+        this.id = id;
+        this.name = name;
+        this.severity = severity;
+        this.age = age;
+    }
+
+    public String toString() {
+        return "ID=" + id + ", Name=" + name +"Severity=" + severity + ", Age=" + age ;
+    }
+
+}
+
